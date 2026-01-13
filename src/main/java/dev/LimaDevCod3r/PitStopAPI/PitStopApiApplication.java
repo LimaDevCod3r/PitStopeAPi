9718e22d60d3777ccd301b80ec6d1cb1f2d92598
Diego Lima <diegolima20100@gmail.com>
2026-01-13 16:14:35 -0300

    frist commit

---
+++ src/main/java/dev/LimaDevCod3r/PitStopAPI/PitStopApiApplication.java
@@ -0,0 +1,13 @@
+package dev.LimaDevCod3r.PitStopAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitStopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PitStopApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/LimaDevCod3r/PitStopAPI/PitStopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.LimaDevCod3r.PitStopAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitStopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
