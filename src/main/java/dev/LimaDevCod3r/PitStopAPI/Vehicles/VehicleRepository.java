b9db8aba538faf6e01fd430442c0c21ec02d4ff4
Diego Lima <diegolima20100@gmail.com>
2026-01-16 09:09:44 -0300

    feat: add JPA repository for Vehicle entity

---
+++ src/main/java/dev/LimaDevCod3r/PitStopAPI/Vehicles/VehicleRepository.java
@@ -0,0 +1,6 @@
+package dev.LimaDevCod3r.PitStopAPI.Vehicles;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VehicleRepository extends JpaRepository<VehicleModel, Long> {
+}
