89b58fd47cebb85da7aade52eac3091af29f0f86
Diego Lima <diegolima20100@gmail.com>
2026-01-20 07:43:09 -0300

    refactor: remove unused import

--- src/main/java/dev/LimaDevCod3r/PitStopAPI/Vehicles/VehicleModel.java
+++ src/main/java/dev/LimaDevCod3r/PitStopAPI/Vehicles/VehicleModel.java
@@ -1,6 +1,5 @@
 package dev.LimaDevCod3r.PitStopAPI.Vehicles;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import dev.LimaDevCod3r.PitStopAPI.Customers.CustomerModel;
 import jakarta.persistence.*;
