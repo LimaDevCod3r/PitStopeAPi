82b87b20007924c24937605bb1fbd6c49608a8d5
Diego Lima <diegolima20100@gmail.com>
2026-01-27 11:12:28 -0300

    feat: add existsByPlate query method

--- src/main/java/dev/LimaDevCod3r/PitStopAPI/Vehicles/VehicleRepository.java
+++ src/main/java/dev/LimaDevCod3r/PitStopAPI/Vehicles/VehicleRepository.java
@@ -3,4 +3,5 @@ package dev.LimaDevCod3r.PitStopAPI.Vehicles;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VehicleRepository extends JpaRepository<VehicleModel, Long> {
+    boolean existsByPlate(String plate);
 }
