048e39f5292d6a1a1fcc92cbb37a03265a407674
Diego Lima <diegolima20100@gmail.com>
2026-01-27 11:14:26 -0300

    feat: map DTOs in service layer and validate plate uniqueness

--- src/main/java/dev/LimaDevCod3r/PitStopAPI/Vehicles/VehicleService.java
+++ src/main/java/dev/LimaDevCod3r/PitStopAPI/Vehicles/VehicleService.java
@@ -1,44 +1,78 @@
 package dev.LimaDevCod3r.PitStopAPI.Vehicles;
 
+import dev.LimaDevCod3r.PitStopAPI.Customers.CustomerModel;
+import dev.LimaDevCod3r.PitStopAPI.Customers.CustomerRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VehicleService {
 
     private final VehicleRepository vehicleRepository;
+    private final CustomerRepository customerRepository;
+    private final VehicleMapper vehicleMapper;
 
-    public VehicleService(VehicleRepository vehicleRepository) {
+    public VehicleService(VehicleRepository vehicleRepository, CustomerRepository customerRepository, VehicleMapper vehicleMapper) {
         this.vehicleRepository = vehicleRepository;
+        this.customerRepository = customerRepository;
+        this.vehicleMapper = vehicleMapper;
     }
 
-    public List<VehicleModel> getAll() {
-        return this.vehicleRepository.findAll();
+    public List<VehicleResponseDTO> getAll() {
+        return this.vehicleRepository.findAll()
+                .stream()
+                .map(this.vehicleMapper::mapToResponse).toList();
     }
 
-    public VehicleModel getById(Long id) {
-        return this.vehicleRepository.findById(id).orElse(null);
+    public VehicleResponseDTO getById(Long id) {
+        Optional<VehicleModel> vehicleById = this.vehicleRepository.findById(id);
+        return vehicleById.map(this.vehicleMapper::mapToResponse).orElse(null);
     }
 
-    public VehicleModel create(VehicleModel vehicleModel) {
-        return this.vehicleRepository.save(vehicleModel);
-    }
+    public VehicleDTO create(VehicleDTO vehicleDTO) {
+        CustomerModel customer = customerRepository.findById(vehicleDTO.getCustomerId()).orElseThrow(
+                () -> new RuntimeException("Customer not found with id: " + vehicleDTO.getCustomerId())
+        );
 
-    public VehicleModel update(Long id, VehicleModel vehicleModel) {
-        VehicleModel vehicle = this.vehicleRepository.findById(id).orElse(null);
-        if (vehicle == null) {
-            return null;
+        if(this.vehicleRepository.existsByPlate(vehicleDTO.getPlate())) {
+            throw new RuntimeException("Vehicle with plate " + vehicleDTO.getPlate() + " already exists");
         }
-        vehicle.setBrand(vehicleModel.getBrand());
-        vehicle.setModel(vehicleModel.getModel());
-        vehicle.setYear(vehicleModel.getYear());
-        vehicle.setColor(vehicleModel.getColor());
-        vehicle.setPlate(vehicleModel.getPlate());
-        return this.vehicleRepository.save(vehicle);
+
+        var entity = this.vehicleMapper.mapToModel(vehicleDTO);
+        entity.setCustomer(customer);
+        var savedVehicle = vehicleRepository.save(entity);
+        return this.vehicleMapper.mapToDTO(savedVehicle);
     }
 
-     public void deleteById(Long id) {
+    public VehicleResponseDTO update(Long id, VehicleDTO vehicleDTO) {
+      VehicleModel vehicleToUpdate = vehicleRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Vehicle not found with id: " + id)
+        );
+
+        if(vehicleDTO.getCustomerId() != null && !vehicleDTO.getCustomerId().equals(vehicleToUpdate.getCustomer().getId())) {
+            CustomerModel customer = customerRepository.findById(vehicleDTO.getCustomerId()).orElseThrow(
+                    () -> new RuntimeException("Customer not found with id: " + vehicleDTO.getCustomerId())
+            );
+            vehicleToUpdate.setCustomer(customer);
+        }
+
+      if(!vehicleDTO.getPlate().equals(vehicleToUpdate.getPlate()) && this.vehicleRepository.existsByPlate(vehicleDTO.getPlate())) {
+          throw new RuntimeException("Vehicle with plate " + vehicleDTO.getPlate() + " already exists");
+      }
+
+
+      vehicleToUpdate.setBrand(vehicleDTO.getBrand());
+      vehicleToUpdate.setModel(vehicleDTO.getModel());
+      vehicleToUpdate.setYear(vehicleDTO.getYear());
+      vehicleToUpdate.setColor(vehicleDTO.getColor());
+      vehicleToUpdate.setPlate(vehicleDTO.getPlate());
+      this.vehicleRepository.save(vehicleToUpdate);
+      return this.vehicleMapper.mapToResponse(vehicleToUpdate);
+    }
+
+    public void deleteById(Long id) {
         this.vehicleRepository.deleteById(id);
     }
 }
