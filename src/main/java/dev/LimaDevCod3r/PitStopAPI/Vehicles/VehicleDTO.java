8b92bddebe9e2f76bcbde08506c03eb1ae5dacd1
Diego Lima <diegolima20100@gmail.com>
2026-01-27 11:14:47 -0300

    feat: add VehicleDTO

---
+++ src/main/java/dev/LimaDevCod3r/PitStopAPI/Vehicles/VehicleDTO.java
@@ -0,0 +1,24 @@
+package dev.LimaDevCod3r.PitStopAPI.Vehicles;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VehicleDTO {
+    private Long id;
+
+    private String brand;
+
+    private Long customerId;
+
+    private String model;
+
+    private String year;
+
+    private  String plate;
+
+    private String color;
+}
